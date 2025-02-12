7de328346e94a1ab19b523348b9db3880635883a
akkimadhava <madhavareddy0205@gmail.com>
2025-02-13 04:52:51 +0530

    initial commit

---
+++ src/main/java/com/dco/customer/DcoCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dco.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcoCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcoCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dco/customer/DcoCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dco.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcoCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
